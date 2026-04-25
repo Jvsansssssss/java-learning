8d799c1ca0da258bb0bee44d9b5aa6e2ad788aa5
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-25 16:11:45 -0300

    Meu primeiro codigo em Java.

---
+++ OComeco.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratona_java.introducao;
+
+/**
+ * Esse é um javadoc
+ */
+public class OComeco {
+    public static void main(String[] args){
+        System.out.println("O começo de tudo.");
+        System.out.println(1.5+3.2);
+        System.out.println("Me chamo João Victor");
+        System.out.println("Tenho " + 16 + " anos");
+    }
+}
