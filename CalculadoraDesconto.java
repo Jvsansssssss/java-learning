b6b09bc19f47628b1f0a99112fb5972d208488d9
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-25 16:21:05 -0300

    feat: adiciona exercicios de tipos primitivos

---
+++ CalculadoraDesconto.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class CalculadoraDesconto {
+    public static void main(String[] args) {
+        String produto = "Computador Gamer";
+        double preco = 3000;
+        int quantidade = 6;
+        boolean promocao = true;
+
+        double subtotal = preco * quantidade;
+        double desconto = 0;
+
+        if(promocao){
+            desconto += subtotal * 0.1;
+        }
+        if(quantidade > 5){
+            desconto +=  subtotal * 0.05;
+        }
+        double precoTotal = subtotal - desconto;
+        System.out.println("----------------------");
+        System.out.println("Produto: " + produto);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("----------------------");
+        System.out.println("Subtotal: R$ " + String.format("%.2f", subtotal));
+        System.out.println("Desconto: R$ " + String.format("%.2f", desconto));
+        System.out.println("Total: R$ " + String.format("%.2f", precoTotal));
+    }
+}
---
+++ TiposPrimitivosExercicios.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "João Victor de Sousa Portela";
+        String endereco = "Rua Neusa Camargo de Oliveira";
+        double salario = 1621.22;
+        String data = "22/12/2026";
+        String historia = "Eu, " + nome + ", morando no endereço: " + endereco + "  Confirmo que recebi o salário de: R$" + salario + " na data: " + data;
+        System.out.println(historia);
+        System.out.println("falso");
+    }
+}
