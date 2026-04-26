1d31a497da77ccabac74b98e1cd82e3b585b44d2
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-26 01:07:02 -0300

    feat: Exercício de controle de estoque usando array e foreach

---
+++ ControleDeEstoque.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class ControleDeEstoque {
+    public static void main(String[] args) {
+        String[] produtos = new String[]{"Mouse" ,"Teclado", "Computador", "Mousepad"};
+        int[] estoque = new int[]{53, 123, 4, 153};
+        for (int i = 0; i < produtos.length; i++){
+            System.out.println("Produto: " + produtos[i] + " - Estoque: " + estoque[i]);
+            System.out.println();
+        }
+        int[] precos = new int[]{200, 320, 3000, 100};
+        double soma = 0;
+        for(int num : precos){
+            soma += num;
+        }
+        System.out.println("Soma total: R$" + String.format("%.2f", soma));
+    }
+}
