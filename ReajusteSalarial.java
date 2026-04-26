5c897e002afde8656fdd7820d452dd9e26cc4298
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-26 01:05:09 -0300

    feat: Exercício de reajuste salarial usando Array

---
+++ ReajusteSalarial.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratona_java.introducao;
+
+public class ReajusteSalarial {
+    public static void main(String[] args) {
+        String[] funcionarios = {"Raphael", "Raí", "João", "Julia"};
+        double[] salarios = {2500, 4322, 4653, 5000};
+        for(int i = 0; i < funcionarios.length; i++){
+            if(salarios[i] < 3000){
+                salarios[i] *= 1.1;
+            }
+            System.out.println("Funcionário: " + funcionarios[i] + " - Salário: " + String.format("%.2f", salarios[i]));
+        }
+    }
+}
