a940d57993495b6a90b58662ffab051e90cae171
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-27 00:37:37 -0300

    feat: implementar construtores

--- orientacao_objetos/aintroducaoclasses/dominio/FerreiroDeArmas.java
+++ orientacao_objetos/aintroducaoclasses/dominio/FerreiroDeArmas.java
@@ -4,4 +4,10 @@ public class FerreiroDeArmas {
     public String nome;
     public int dano;
     public String tipo;
+
+    public FerreiroDeArmas(String nome, int dano, String tipo){
+        this.nome = nome;
+        this.dano = dano;
+        this.tipo = tipo;
+    }
 }
--- orientacao_objetos/aintroducaoclasses/test/FerreiroDeArmasTest.java
+++ orientacao_objetos/aintroducaoclasses/test/FerreiroDeArmasTest.java
@@ -4,14 +4,8 @@ import aintroducaoclasses.dominio.FerreiroDeArmas;
 
 public class FerreiroDeArmasTest {
     public static void main(String[] args) {
-        FerreiroDeArmas armaTest01 = new FerreiroDeArmas();
-        FerreiroDeArmas armaTest02 = new FerreiroDeArmas();
-        armaTest01.nome = "Excalibur";
-        armaTest01.dano = 100;
-        armaTest01.tipo = "Espada Lendária";
-        armaTest02.nome = "Kusanagi";
-        armaTest02.dano = 170;
-        armaTest02.tipo = "Katana Lendária";
+        FerreiroDeArmas armaTest01 = new FerreiroDeArmas("Excalibur", 100, "Espada Lendária");
+        FerreiroDeArmas armaTest02 = new FerreiroDeArmas("Kusanagi", 170, "Katana Lendária");
 
         System.out.println("Arma: " + armaTest01.nome + " - Dano: " + armaTest01.dano + " - Tipo: " + armaTest01.tipo);
         System.out.println("Arma: " + armaTest02.nome + " - Dano: " + armaTest02.dano + " - Tipo: " + armaTest02.tipo);
