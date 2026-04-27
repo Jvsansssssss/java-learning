c61f790ace77dc5ee12bda5f373493d8b4841ca8
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-27 01:04:48 -0300

    feat: implementar calculadora de dano com retorno

--- orientacao_objetos/bintroducaometodos/dominio/CalculadoraDeDano.java
+++ orientacao_objetos/bintroducaometodos/dominio/CalculadoraDeDano.java
@@ -1,7 +1,7 @@
 package bintroducaometodos.dominio;
 
 public class CalculadoraDeDano {
-    public double calcularMediaDano(double dano1, double dano2, double dano3){
+    public double calcMediaDano(double dano1, double dano2, double dano3){
         return (dano1 + dano2 + dano3) / 3;
     }
 }
--- orientacao_objetos/bintroducaometodos/test/CalculadoraDeDanoTest.java
+++ orientacao_objetos/bintroducaometodos/test/CalculadoraDeDanoTest.java
@@ -5,7 +5,7 @@ import bintroducaometodos.dominio.CalculadoraDeDano;
 public class CalculadoraDeDanoTest {
     public static void main(String[] args) {
         CalculadoraDeDano calc = new CalculadoraDeDano();
-        double resultado = calc.calcularMediaDano(6.23, 54, 123);
-        System.out.println("A média de dano das 3 armas é: " + String.format("%.2f", resultado));
+        double resultado = calc.calcMediaDano(6.23, 54, 123);
+        System.out.println("A média do dano das 3 armas é: " + String.format("%.2f", resultado));
     }
 }
