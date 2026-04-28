8671e691deaa095947701c6e17cc200c6f4be16f
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-28 00:02:06 -0300

    feat: calculadora usando array e VarArgs

--- orientacao_objetos/bintroducaometodos/dominio/Calculadora.java
+++ orientacao_objetos/bintroducaometodos/dominio/Calculadora.java
@@ -4,4 +4,20 @@ public class Calculadora {
     public void soma(){
         System.out.println(10 + 14);
     }
+    public void somaArray(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+
+        }
+        System.out.println(soma);
+    }
+    public void somaVarAgs(int... numeros){
+        int soma = 0;
+        for (int num: numeros) {
+            soma += num;
+            
+        }
+        System.out.println(soma);
+    }
 }
--- orientacao_objetos/bintroducaometodos/test/CalculadoraTest.java
+++ orientacao_objetos/bintroducaometodos/test/CalculadoraTest.java
@@ -6,5 +6,9 @@ public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         calculadora.soma();
+        int[] numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarAgs(1,2,3,4,5,6,7,8,9,10);
     }
+
 }
