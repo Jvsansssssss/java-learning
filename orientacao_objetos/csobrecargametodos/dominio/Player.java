8dbcda9f2ef9f7905ea7348909ecda9b2cccf64c
Jvsansssssss <joaovictorxportela@gmail.com>
2026-04-28 17:15:05 -0300

    feat: adicionar classe Player com sobrecarga de métodos e encadeamento

--- orientacao_objetos/csobrecargametodos/dominio/Player.java
+++ orientacao_objetos/csobrecargametodos/dominio/Player.java
@@ -1,4 +1,32 @@
 package csobrecargametodos.dominio;
 
 public class Player {
+    private String nome;
+    private String classe;
+    private int nivel;
+    private String servidor;
+
+    public void setup(String nome, String classe, int nivel, String servidor){
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.servidor = servidor;
+    }
+
+    public void setup(String nome, String classe){
+        this.setup(nome, classe, 1, "Brasil");
+    }
+
+    public void setup(String nome){
+        this.setup(nome, "Aprendiz");
+    }
+
+
+
+    public void imprime(){
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua classe é: " + classe);
+        System.out.println("Seu nível é: " + nivel);
+        System.out.println("Seu servidor é: " + servidor);
+    }
 }
--- orientacao_objetos/csobrecargametodos/test/PlayerTest.java
+++ orientacao_objetos/csobrecargametodos/test/PlayerTest.java
@@ -1,4 +1,18 @@
 package csobrecargametodos.test;
 
+import csobrecargametodos.dominio.Player;
+
 public class PlayerTest {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.setup("Raí");
+        player.imprime();
+        System.out.println("-----------");
+        player.setup("João", "Clérigo");
+        player.imprime();
+        System.out.println("-----------");
+        player.setup("Julia", "Mago", 10, "Brasil");
+        player.imprime();
+
+    }
 }
