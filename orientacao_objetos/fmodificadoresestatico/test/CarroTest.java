5bb52eedc5fa09d2f7e9f5f1f061e7f4c7c156ab
Jvsansssssss <joaovictorxportela@gmail.com>
2026-05-01 00:00:42 -0300

    feat: exercicio de velocidade limite usando static

--- orientacao_objetos/fmodificadoresestatico/test/CarroTest.java
+++ orientacao_objetos/fmodificadoresestatico/test/CarroTest.java
@@ -1,4 +1,15 @@
 package fmodificadoresestatico.test;
 
-public class CarroTesdt {
+import fmodificadoresestatico.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Lamborghini", 260);
+        Carro carro2 = new Carro("BMW", 300);
+        Carro carro3 = new Carro("Ferrari", 320);
+        Carro.velocidadeLimite = 180;
+        carro1.imprime();
+        carro2.imprime();
+        carro3.imprime();
+    }
 }
